7771a75ea6afb947361101ff4ef5eb80af6cc4e7
Tomasz <tjagodzinski94@gmail.com>
2025-01-22 22:27:04 +0100

    Refactored TaskList component to accept a prop for receiving the task list dynamically.

--- src/components/task/TaskList.js
+++ src/components/task/TaskList.js
@@ -1,24 +1,9 @@
 import TaskItem from "./TaskItem";
 
-const TASK_DUMMY = [
-  {
-    text: "Task 1",
-    date: "12/01/2025",
-  },
-  {
-    text: "Task 2",
-    date: "12/02/2025",
-  },
-  {
-    text: "Task 3",
-    date: "12/03/2025",
-  },
-];
-
-export default function TaskList() {
+export default function TaskList({ taskList }) {
   return (
     <ul>
-      {TASK_DUMMY.map((task) => (
+      {taskList.map((task) => (
         <TaskItem key={task.text} task={task} />
       ))}
     </ul>
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,11 +1,29 @@
 import TaskAdd from "../components/task/TaskAdd";
 import TaskList from "../components/task/TaskList";
 
+const TASK_DUMMY = [
+  {
+    text: "Task 1",
+    date: "12/01/2025",
+    status: "to-do",
+  },
+  {
+    text: "Task 2",
+    date: "12/02/2025",
+    status: "to-do",
+  },
+  {
+    text: "Task 3",
+    date: "12/03/2025",
+    status: "to-do",
+  },
+];
+
 export default function HomePage() {
   return (
     <>
       <TaskAdd />
-      <TaskList />
+      <TaskList taskList={TASK_DUMMY} />
     </>
   );
 }
