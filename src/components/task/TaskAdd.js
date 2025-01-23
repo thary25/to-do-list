8ecfa6493bf15a3532e98259f60a0fdfe5e16e2a
Tomasz <tjagodzinski94@gmail.com>
2025-01-23 18:39:30 +0100

    Enabled task addition functionality and integrated useState for managing the task list dynamically.

--- src/components/task/TaskAdd.js
+++ src/components/task/TaskAdd.js
@@ -1,11 +1,24 @@
+import { useRef } from "react";
 import classes from "./TaskAdd.module.css";
 
-export default function TaskAdd() {
+export default function TaskAdd({ action }) {
+  const ref = useRef();
   return (
     <>
       <form className={classes.form}>
-        <input type="text" name="name" id="name" placeholder="Add Item" />
-        <button>Add</button>
+        <input
+          ref={ref}
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Add Item"
+        />
+        <button
+          type="button"
+          onClick={() => action(ref.current.value, "12/01/2025")}
+        >
+          Add
+        </button>
       </form>
     </>
   );
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,33 +1,23 @@
+import { useState } from "react";
 import TaskAdd from "../components/task/TaskAdd";
 import TaskList from "../components/task/TaskList";
 import { HeadingH3 } from "../components/ui/Heading";
 import Section from "../components/ui/Section";
 
-const TASK_DUMMY = [
-  {
-    text: "Task 1",
-    date: "12/01/2025",
-    status: "to-do",
-  },
-  {
-    text: "Task 2",
-    date: "12/02/2025",
-    status: "to-do",
-  },
-  {
-    text: "Task 3",
-    date: "12/03/2025",
-    status: "to-do",
-  },
-];
-
 export default function HomePage() {
+  function addTaskHandler(text, date, status = "to-do") {
+    setTaskList((prevState) => {
+      return [...prevState, { text, date, status }];
+    });
+  }
+
+  const [taskList, setTaskList] = useState([]);
   return (
     <>
       <Section>
-        <TaskAdd />
+        <TaskAdd action={addTaskHandler} />
         <HeadingH3>To Do</HeadingH3>
-        <TaskList taskList={TASK_DUMMY} />
+        <TaskList taskList={taskList} />
       </Section>
     </>
   );
